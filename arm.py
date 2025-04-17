09b0b04cae50d7b36d9c407a000550f01e189edb
lythienne <harrisonlinchen@gmail.com>
2025-04-17 13:21:03 -0700

    Arm Simulator forward kinematics using homogenous coordinates and PoE

--- arm.py
+++ arm.py
@@ -1,13 +1,22 @@
 import numpy as np
+import scipy as sp
 
 # lengths
 L5 = 1      # gripper
 L3 = 0.6    # forearm
 L2 = 1.05   # upper arm
 
-origin = np.array([0, 0, 0.8])
+lengths = {
+    0: 0,
+    1: L2,
+    2: L2 + L3,
+    3: L2 + L3 + L5
+}
+
+origin = np.array([0, 0, 0, 1])
 
 
+# @Deprecated
 def sph2rect(rho, phi, theta):
     r = rho*np.sin(phi)
     z = rho*np.cos(phi)
@@ -16,6 +25,7 @@ def sph2rect(rho, phi, theta):
     return np.array([x, y, z])
 
 
+# @Deprecated
 def gripper_rotation(x_g, y_g, z_g, phi, theta):
     x_temp = x_g*np.cos(phi)+z_g*np.sin(phi)
     z = -x_g*np.sin(phi)+z_g*np.cos(phi)
@@ -26,6 +36,7 @@ def gripper_rotation(x_g, y_g, z_g, phi, theta):
     return np.array([x, y, z])
 
 
+# @Deprecated
 # dof is a list of angles [-pi/2, pi/2]
 def armPoints(dof):
     p1 = sph2rect(L2, dof[0], dof[4]) + origin
@@ -38,4 +49,45 @@ def armPoints(dof):
 
     p3 = gripper_rotation(g[0], g[1], g[2], phi, theta) + p2
     p4 = gripper_rotation(-g[0], -g[1], g[2], phi, theta) + p2
+    return [origin, p1, p2, p3, p4]
+
+
+def s3_in_brackets(n):
+    return np.array([
+        [0, -1, 0, lengths[n]],
+        [1, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ])
+
+
+def M(n):
+    return np.array([
+        [1, 0, 0, 0],
+        [0, 1, 0, lengths[n]],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ])
+
+
+def armPointsCoolerVersion(dof):
+    wrist = np.array([
+        [0, 0, 1, 0],
+        [0, 0, 0, 0],
+        [-1, 0, 0, 0],
+        [0, 0, 0, 0]
+    ])
+
+    t0 = sp.linalg.expm(wrist*dof[4])
+    t1 = t0 @ sp.linalg.expm(s3_in_brackets(0)*dof[0])
+    t2 = t1 @ sp.linalg.expm(s3_in_brackets(1)*dof[1])
+    wrist_twist = t2 @ sp.linalg.expm(wrist*dof[2])
+    t3 = wrist_twist @ sp.linalg.expm(s3_in_brackets(2)*dof[3])
+    t4 = wrist_twist @ sp.linalg.expm(s3_in_brackets(2)*-dof[3])
+
+    p4 = t4 @ M(3) @ origin
+    p3 = t3 @ M(3) @ origin
+    p2 = t2 @ M(2) @ origin
+    p1 = t1 @ M(1) @ origin
+
     return [origin, p1, p2, p3, p4]
--- plot.py
+++ plot.py
@@ -2,7 +2,7 @@ from numpy import pi
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button
-from arm import armPoints
+from arm import armPoints, armPointsCoolerVersion
 
 # Create the figure and the line that we will manipulate
 fig, ax = plt.subplot_mosaic([['up'], ['down']])
@@ -14,7 +14,7 @@ def update_ax(arm_points):
         graph.set_xlim([-3, 3])
         graph.set_aspect('equal')
 
-    ax['up'].set_ylim([0, 4])
+    ax['up'].set_ylim([-3, 3])
     ax['up'].set_title("side")
 
     ax['down'].set_ylim([-3, 3])
@@ -24,18 +24,18 @@ def update_ax(arm_points):
     arm_y = list(pt[1] for pt in arm_points)
     arm_z = list(pt[2] for pt in arm_points)
 
-    ax['up'].plot(arm_x[:-1], arm_z[:-1], 'gray')
-    ax['up'].plot([arm_x[2], arm_x[4]], [arm_z[2], arm_z[4]], 'gray')
-    ax['up'].scatter(arm_x, arm_z, 2, 'gray')
+    ax['up'].plot(arm_x[:-1], arm_y[:-1], 'gray')
+    ax['up'].plot([arm_x[2], arm_x[4]], [arm_y[2], arm_y[4]], 'gray')
+    ax['up'].scatter(arm_x, arm_y, 2, 'gray')
 
-    ax['down'].plot(arm_x[:-1], arm_y[:-1], 'gray')
-    ax['down'].plot([arm_x[2], arm_x[4]], [arm_y[2], arm_y[4]], 'gray')
-    ax['down'].scatter(arm_x, arm_y, 2, 'gray')
+    ax['down'].plot(arm_x[:-1], arm_z[:-1], 'gray')
+    ax['down'].plot([arm_x[2], arm_x[4]], [arm_z[2], arm_z[4]], 'gray')
+    ax['down'].scatter(arm_x, arm_z, 2, 'gray')
 
 
-update_ax([np.array([0, 0, 0.8]), np.array([0, 0, 1.85]),
-           np.array([0, 0, 2.45]),
-           np.array([0, 0, 3.45]), np.array([0, 0, 3.45])])
+update_ax([np.array([0, 0.8, 0]), np.array([0, 1.85, 0]),
+           np.array([0, 2.45, 0]),
+           np.array([0, 3.45, 0]), np.array([0, 3.45, 0])])
 
 # adjust the main plot to make room for the sliders
 fig.subplots_adjust(left=0.6, bottom=0.15, hspace=0.4)
@@ -45,8 +45,8 @@ ax_base = fig.add_axes([0.45, 0.25, 0.0225, 0.63])
 base_angle = Slider(
     ax=ax_base,
     label='Base',
-    valmin=-90,
-    valmax=90,
+    valmin=0,
+    valmax=180,
     valinit=0,
     orientation="vertical"
 )
@@ -98,11 +98,11 @@ gripper_angle = Slider(
 
 # The function to be called anytime a slider's value changes
 def update(val):
-    arm_points = armPoints([shoulder_angle.val/180*pi,
-                            elbow_angle.val/180*pi,
-                            wrist_angle.val/180*pi+pi/2,
-                            gripper_angle.val/180*pi,
-                            base_angle.val/180*pi+pi/2])
+    arm_points = armPointsCoolerVersion([shoulder_angle.val/180*pi,
+                                         elbow_angle.val/180*pi,
+                                         wrist_angle.val/180*pi+pi/2,
+                                         gripper_angle.val/180*pi,
+                                         base_angle.val/180*pi+pi/2])
     update_ax(arm_points)
     fig.canvas.draw_idle()
 
